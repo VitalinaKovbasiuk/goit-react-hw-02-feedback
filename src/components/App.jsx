407171e180926e27297695da8e414a7c39729959
Vitalina Kovbasiuk <kovbasiukvitalina@gmail.com>
2022-10-26 20:08:58 +0300

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
+import "./App.css";
+import Feedback from "components/Feedback/Feedback";
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <Feedback />
     </div>
   );
-};
+}
+
+export default App;
